1518e7fc26b938b48fdb382770651896fbaffc48
anooppandikashala <anooppandikashala@gmail.com>
2025-07-18 16:20:08 +0530

    added app constants

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+
+// constants.dart
+import 'package:flutter/material.dart';
+
+class AppSpacing {
+  static const baseValue = 5.0;
+  static const hSmall = SizedBox(height: baseValue);
+  static const hMedium = SizedBox(height: 2*baseValue);
+  static const hLarge = SizedBox(height: 3*baseValue);
+  static const hXlarge = SizedBox(height: 4*baseValue);
+
+  static const wSmall = SizedBox(width: baseValue);
+  static const wMedium = SizedBox(width: 2*baseValue);
+  static const wLarge = SizedBox(width: 3*baseValue);
+  static const wXlarge = SizedBox(width: 4*baseValue);
+}
