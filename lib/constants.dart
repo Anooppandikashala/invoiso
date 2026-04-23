c1245ee63b3fe43e7e2e621976a8d23d917aa7b5
anooppandikashala <anooppandikashala@gmail.com>
2026-04-23 10:12:55 +0530

    v4.0.4

--- lib/constants.dart
+++ lib/constants.dart
@@ -64,7 +64,7 @@ class AppBorderRadius
 class AppConfig
 {
   static const name = "invoiso";
-  static const version = "v4.0.3";
+  static const version = "v4.0.4";
   static const developer = "ANOOP P";
   static const supportEmail = "anooppkrishnan96@gmail.com";
   static const website = "https://anooppandikashala.github.io/invoisoapp/";
